9a5e6fe38eed71524db1e46268315fb23d2d6e99
Shaylee Chong <shayleechong@gmail.com>
2018-04-24 11:15:52 +0800

    Updates

---
+++ Quizzler/Question.swift
@@ -0,0 +1,22 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Shaylee Chong on 20/4/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question{
+    
+    let questionText : String
+    let answer : Bool
+    
+    init(text: String, correctAnswer: Bool){
+        questionText = text
+        answer = correctAnswer
+    }
+}
+
+
--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -1,17 +1,12 @@
-//
-//  ViewController.swift
-//  Quizzler
-//
-//  Created by Angela Yu on 25/08/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool =  false
+    var questionNumber : Int = 1
+    var scoreCount : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,31 +16,83 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
+        scoreLabel.text = "Score: " + String(scoreCount)
+        progressLabel.text = String(questionNumber) + "/13"
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
     }
     
     
     func updateUI() {
-      
+        
+      questionLabel.text = allQuestions.list[questionNumber].questionText
+      progressLabel.text = String(questionNumber) + "/13"
+        scoreLabel.text = "Score: " + String(scoreCount)
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber)
+        
     }
     
 
     func nextQuestion() {
+        questionNumber += 1
+        
+        if allQuestions.list.count == questionNumber {
+            progressLabel.text = String(questionNumber) + "/13"
+            scoreLabel.text = "Score: " + String(scoreCount)
+            progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber)
+            let alert = UIAlertController(title: "The End", message: "We have reach the end of the quiz :) Your score is \(String(scoreCount)) out of 13!", preferredStyle: .alert)
+            
+            let action2 = UIAlertAction(title: "Restart", style: .default) { (action:UIAlertAction) in
+                self.startOver()
+            }
+
+            alert.addAction(action2)
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        }else{
+            updateUI()
+        }
+        
+        
         
     }
     
     
     func checkAnswer() {
         
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        
+        if correctAnswer == pickedAnswer {
+            print("you got it")
+            scoreCount += 1
+        } else {
+            print("wrong")
+        }
+        
+        nextQuestion()
+        
     }
     
     
     func startOver() {
-       
+        
+       questionNumber = 0
+       scoreCount = 0
+       updateUI()
+        
     }
     
 
